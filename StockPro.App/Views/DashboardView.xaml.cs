0dd4b32605d71fd85f9d218238066d23f5fd17b4
yusa.turan <yusa.turan@iski.gov.tr>
2026-09-15 08:30:05 +0300

    devam

---
+++ StockPro.App/Views/DashboardView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace StockPro.App.Views;
+
+public partial class DashboardView : UserControl
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+}
--- StockPro.Infrastructure/obj/Debug/net9.0/StockPro.Infrastructure.AssemblyInfo.cs
+++ StockPro.Infrastructure/obj/Debug/net9.0/StockPro.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6066ce2d9444d11f935c0239b3f001efa7ca634c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f516e5ac609f36024929d43db0092ad49685bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockPro.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
